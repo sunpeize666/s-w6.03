865f7eeb01f71fc0bea058bc17cbf1ca267d9abe
sunpeize666 <144121626+sunpeize666@users.noreply.github.com>
2024-06-03 10:36:13 +0900

    Add files via upload

---
+++ 5.31/5.31_01_손패택.py
@@ -0,0 +1,14 @@
+'''
+2024년05월31일
+202195057 손패택
+'''
+def sum(num1,num2) :
+    #result = num1 + num2
+    # return result
+    return num1 + num2
+
+num1 = int(input("첫 번째 정수 입력:"))
+num2 = int(input("두 번째 정수 입력:"))
+
+reult = sum(num1,num2)
+
